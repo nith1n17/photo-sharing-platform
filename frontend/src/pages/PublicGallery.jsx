bc2be0a085fd73a7454441c5e3ab829a3ea37908
nith1n17 <nithinsv17@gmail.com>
2026-09-13 22:56:24 +0530

    Fix gallery API import

--- frontend/src/pages/PublicGallery.jsx
+++ frontend/src/pages/PublicGallery.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
-import axios from "axios";
+import api from "../services/api";
 
 function PublicGallery() {
   const { galleryToken } = useParams();
